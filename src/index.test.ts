5b56f3f29063a4a90ec5df141ecd3a9f2afd7545
Mykhailo.Romaniuk <mykhailo.romaniuk@eleks.com>
2022-02-03 00:33:46 +0200

    Install and init eslint

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,7 +2,7 @@ import { add } from '.';
 
 describe('test', () => {
   it('WHEN adding two values SHOULD return sum', () => {
-    const expected: number = 4;
+    const expected = 4;
 
     const actual = add(2, 2);
 
